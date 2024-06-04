05d2541ba155e56c926fc209da2ba1bed6e5218d
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-06-04 06:31:18 -0300

    Added docker support

--- core/views.py
+++ core/views.py
@@ -10,9 +10,6 @@ import xarray as xr
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from osgeo import gdal
-from folium.plugins import HeatMap
-
 from django.views.generic import TemplateView
 from django.contrib.gis.db.models.functions import Transform
 from django.db.models import Min, Max, Avg
@@ -24,70 +21,13 @@ logger = logging.getLogger(__name__)
 colormap = cm.linear.Paired_07.scale(-2, 35)
 
 
-def color_map_test(*args):
-    if args[0] == -32767 or np.isnan(args[0]):
-        return 0, 0, 0, 0
-
-    return colormap.rgba_floats_tuple(args[0])
-
-
-def add_mpa_raster_gdal(folium_map):
-    ds = gdal.Open("scripts/data/mpa_clipped.tif")
-    img = ds.GetRasterBand(1).ReadAsArray()
-    GT = ds.GetGeoTransform()
-    xRes = ds.GetGeoTransform()[1]
-    yRes = ds.GetGeoTransform()[5]
-    width = ds.RasterXSize
-    height = ds.RasterYSize
-    folium.raster_layers.ImageOverlay(
-        image=np.flip(img, 0),
-        opacity=0.5,
-        bounds=[[GT[3] + height * yRes, GT[0]], [GT[3], GT[0] + width * xRes]],
-        colormap=lambda x: (x, 0, 0, 0 if x == -32767 else 1),
-        crs="EPSG:3857",
-    ).add_to(folium_map)
-
-
-def add_map_raster_rio(raster_location, folium_map):
-    with rioxarray.open_rasterio(raster_location) as src:
-        img = src.data
-        src_min_lon, src_min_lat, src_max_lon, src_max_lat = src.rio.bounds()
-
-    src_bounds_orig = [[src_min_lat, src_min_lon], [src_max_lat, src_max_lon]]
-
-    folium_map.add_child(colormap)
-
-    folium.raster_layers.ImageOverlay(
-        image=np.flip(img[0], 0),
-        opacity=.8,
-        bounds=src_bounds_orig,
-        mercator_project=True,
-        colormap=color_map_test,
-    ).add_to(folium_map)
-
-
-def add_heat_map(file, folium_map):
-    ncdf_data = xr.load_dataset(file)
-    temp = ncdf_data.thetao.isel(time=0, depth=0)
-    temp_df = temp.to_dataframe()[['thetao']].dropna()
-
-    HeatMap(
-        [(index[0], index[1], row['thetao']) for index, row in temp_df.iterrows()],
-        name="MPA Temperature",
-        min_opacity=0.2,
-        radius=20,
-        blur=15,
-        max_zoom=1
-    ).add_to(folium_map)
-
-
 def add_attributes(mpa):
 
     temp = 20
     geo_json = {
         "type": "Feature",
         "style": {
-            "color": mpl.colors.rgb2hex(color_map_test(temp), keep_alpha=False),
+            "color": "#FF5555",
         },
         "id": mpa.pk,
         "properties": {
