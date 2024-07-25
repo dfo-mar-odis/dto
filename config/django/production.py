198a80b1d31cfffdd5d4940494808b90d5a72f32
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-07-25 06:22:01 -0300

    adding GitHub Actions to build docker image

--- config/django/production.py
+++ config/django/production.py
@@ -3,4 +3,4 @@ from config.env import env
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
