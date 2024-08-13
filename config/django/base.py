37dd58d4af0926a050d9ef02440346836ebe2c54
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-13 13:52:30 -0300

    updated static url

--- config/django/base.py
+++ config/django/base.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = 'webapp-DTO/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
