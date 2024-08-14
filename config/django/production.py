6e93c1d12d32ec30337de44ad2ce47a19cd912cb
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-14 10:31:00 -0300

    added collectstatic to entry-point.sh script

--- config/django/production.py
+++ config/django/production.py
@@ -2,6 +2,6 @@ from .base import *
 from config.env import env
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
-STATIC_URL = env.str('STATIC_URL', 'webapp-DTO/static/')
+STATIC_URL = env.str('STATIC_URL', 'static/')
 
 # ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
--- entry-point.sh
+++ entry-point.sh
@@ -5,4 +5,5 @@
 
 #python manage.py check
 python manage.py migrate
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
