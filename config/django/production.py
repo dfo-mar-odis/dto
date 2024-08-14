35a0f037f58f3beeb25a7ae9adea29f9670cf585
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-14 10:07:47 -0300

    changed static url in production.py

--- config/django/production.py
+++ config/django/production.py
@@ -2,5 +2,6 @@ from .base import *
 from config.env import env
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
+STATIC_URL = env.str('STATIC_URL', 'webapp-DTO/static/')
 
 # ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
