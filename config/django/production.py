437d265c92a53b5f8e2f9797b74bed5b88f55271
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-21 06:52:30 -0300

    Fixing FSDH docker env issue

--- config/django/production.py
+++ config/django/production.py
@@ -5,7 +5,7 @@ from config.env import env
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 # https://poc.fsdh-dhsf.science.cloud-nuage.canada.ca/webapp-DTO/static/core/js/range_chart.js
-STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'https://poc.fsdh-dhsf.science.cloud-nuage.canada.ca/webapp-DTO/static/'))
-# STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'static/'))
+# STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'https://poc.fsdh-dhsf.science.cloud-nuage.canada.ca/webapp-DTO/static/'))
+STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'staticfiles/'))
 
 # ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[]
