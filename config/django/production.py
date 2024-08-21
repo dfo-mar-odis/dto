b112de73202d2b77e12dc52f385c5b8a28b7bc00
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-21 06:32:14 -0300

    Fixing FSDH docker env issue

--- config/django/production.py
+++ config/django/production.py
@@ -4,6 +4,7 @@ from .base import *
 from config.env import env
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
-STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'https://poc.fsdh-dhsf.science.cloud-nuage.canada.ca/webapp-DTO/static/'))
+# STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'https://poc.fsdh-dhsf.science.cloud-nuage.canada.ca/webapp-DTO/static/'))
+STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'static/'))
 
 # ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[]
