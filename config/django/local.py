87bd816edae9467867ed84be2df7cfc9e86992be
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-08-21 09:32:58 -0300

    Fixing FSDH docker env issue

--- config/django/local.py
+++ config/django/local.py
@@ -2,3 +2,4 @@ from .base import *
 
 # GDAL_LIBRARY_PATH = r'D:\Gov\projects\python\dto_env\Lib\site-packages\osgeo\gdal304.dll'
 # GEOS_LIBRARY_PATH = r'D:\Gov\projects\python\dto_env\Lib\site-packages\osgeo\geos_c.dll'
+STATIC_URL = env.str('STATIC_URL', os.getenv('STATIC_URL',  'https://www.bio.gc.ca/dto/staticfiles/'))
