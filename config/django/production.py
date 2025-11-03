e4f0325f9bc0c9380051e746d40777990a35fcd6
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-11-03 15:41:40 -0400

    Letting debug be handled by env variables

--- config/django/production.py
+++ config/django/production.py
@@ -3,7 +3,6 @@ import os
 from .base import *
 from config.env import env
 
-DEBUG = env.bool('DJANGO_DEBUG', default=True)
 FORCE_SCRIPT_NAME = "/app/DTO"
 STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 
